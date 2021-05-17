f52cc0bd42e280205ae407c63d00bdb5f33a43e4
Justin Goh <justin.goh@thoughtworks.com>
2021-05-17 16:19:03 +0800

    add spring boot initializr gradle

---
+++ src/test/java/com/example/buildkitetest/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buildkitetest.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
